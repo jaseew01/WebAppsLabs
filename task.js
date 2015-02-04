56913770d2f8a44c38e2aafec5356f35195ebcc4
jaseew01 <jseewer@twc.com>
2015-02-04 17:00:51 -0500

    fixed eslint problems

--- task.js
+++ task.js
@@ -1,55 +1,40 @@
 /*
- * task.js
- *
- * Contains implementation for a "task" "class"
- */
-
-var Task ;
-var proto;
-
+* task.js
+*
+* Contains implementation for a "task" "class"
+*/
+var Task, proto;
 // Helper method. You should not need to change it.
 // Use it in makeTaskFromString
 function processString(s) {
    "use strict";
    var tags, title;
-
    tags = [];
    title = s.replace(/\s*#([a-zA-Z]+)/g, function(m, tag) {
       tags.push(tag);
       return "";
    });
-
    return { title: title, tags: tags };
 }
 
 /*
- *       Constructors
- */
-
-function makeNewTask () {
-
-};
-
-function makeTaskFromObject( o )
- {
-
+* Constructors
+*/
+function makeNewTask() {
+   "use strict";
 }
-
-function makeTaskFromString(str)
-{}
-
-
+function makeTaskFromObject(o) {
+   "use strict";
+}
+function makeTaskFromString(str) {
+   "use strict";
+}
 /*
- *       Prototype / Instance methods
- */
-
-proto={
-   //Add instance methods here
-
-}
-
-
-
+* Prototype / Instance methods
+*/
+proto = {
+// Add instance methods here
+};
 // DO NOT MODIFY ANYTHING BELOW THIS LINE
 Task = {
    new: makeNewTask,
