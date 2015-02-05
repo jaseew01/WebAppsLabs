220a70fb87fb98e95d2d88e7f3c03f62d008d449
jaseew01 <jseewer@twc.com>
2015-02-04 19:40:08 -0500

    finished prototype methods and contructors, passes eslint

--- task.js
+++ task.js
@@ -17,23 +17,152 @@ function processString(s) {
    return { title: title, tags: tags };
 }
 
+function idCounter() {
+   "use strict";
+   var num = 0;
+   return function() {
+      num += 1;
+      return num;
+   };
+}
+
 /*
 * Constructors
 */
 function makeNewTask() {
    "use strict";
+   var temp = Object.create(null), arr = [];
+   temp.title = "";
+   temp.completedTime = null;
+
+   Object.defineProperty(temp, "id", {
+      enumerable: true,
+      configurable: false,
+      writable: false,
+
+      get: function() {
+         return idCounter();
+      }
+   });
+   Object.defineProperty(temp, "tags", {
+      get: function() {
+         return arr;
+      }
+   });
+
+   Object.preventExtensions();
+   return temp;
 }
+
 function makeTaskFromObject(o) {
    "use strict";
+   var newTask = Task.new;
+   newTask.setTitle(o.title);
+   newTask.addTags(o.tags);
+   return newTask;
 }
-function makeTaskFromString(str) {
+
+function makeTaskFromString(s) {
    "use strict";
+    return Task.fromObject(processString(s));
 }
+
 /*
 * Prototype / Instance methods
 */
 proto = {
-// Add instance methods here
+   setTitle: function(s) {
+      "use strict";
+      this.title = s.trim();
+      return this;
+   },
+
+   isCompleted: function() {
+      "use strict";
+      if (this.completedTime == null) {
+         return false;
+      }
+      return true;
+   },
+
+   toggleCompleted: function() {
+      "use strict";
+      var date = new Date();
+      if (this.completedTime == null) {
+         this.completedTime = date;
+      } else {
+         this.completedTime = null;
+      }
+      return this;
+   },
+
+   hasTag: function(s) {
+      "use strict";
+      if (this.tags.includes(s)) {
+         return true;
+      }
+      return false;
+   },
+
+   addTag: function(s) {
+      "use strict";
+      if (this.hasTag(s) === false) {
+         this.tags.push(s);
+      }
+      return this;
+   },
+
+   removeTag: function(s) {
+      "use strict";
+      var temp = this.tags.indexOf(s);
+      if (temp !== -1) {
+         this.tags.splice(temp, 1);
+      }
+      return this;
+   },
+
+   toggleTag: function(s) {
+      "use strict";
+      if (this.hasTag(s)) {
+         this.removeTag(s);
+      } else {
+         this.addTag(s);
+      }
+      return this;
+   },
+
+   addTags: function(arr) {
+      "use strict";
+      arr.forEach(function(v, i) {
+         this.addTag(v);
+      });
+      return this;
+   },
+
+   removeTags: function(arr) {
+      "use strict";
+      arr.forEach(function(v, i) {
+         this.removeTag(v);
+      });
+      return this;
+   },
+
+   toggleTags: function(arr) {
+      "use strict";
+      arr.forEach(function(v, i) {
+         this.toggleTag(v);
+      });
+      return this;
+   },
+
+   clone: function() {
+      "use strict";
+      var newObj = new makeNewTask();
+      newObj.title = this.title;
+      newObj.completedTime = this.completedTime;
+      newObj.tags = this.tags;
+      return newObj;
+   }
 };
 // DO NOT MODIFY ANYTHING BELOW THIS LINE
 Task = {
