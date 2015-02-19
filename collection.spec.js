97d924fb9fd7ffce4e511b2508088dbf3621b8e3
jaseew01 <jseewer@twc.com>
2015-02-19 18:45:55 -0500

    Finished tests for get method (Issue #17(

--- collection.spec.js
+++ collection.spec.js
@@ -47,3 +47,41 @@ describe("Testing collection prototype methods", function() {
 		expect(myCollection.isEmpty()).to.equal(true);
 		expect(myCollection2.isEmpty()).to.equal(false);
 	});
+	it("Testing the get method", function() {
+		var myfunc = function(arg) {
+			if(arg.title === "test2") {
+				return true;
+			}
+			return false;
+		};
+		expect(myCollection3.get(myfunc).title).to.equal("test2");
+		expect(myCollection3.get(1).title).to.equal("test");
+		expect(myCollection3.get("test").id).to.equal(1);
+		expect(myCollection3.get(/test2/).id).to.equal(2);
+		expect(myCollection3.get(3)).to.equal(null);
+	});
+	it("Testing the has method", function() {
+		//
+	});
+	it.skip("Testing the add method", function() {
+		expect(myCollection3.length()).to.equal(2);
+		myCollection3.add(task1);
+		expect(myCollection3.length()).to.equal(3);
+		myCollection3.add([task1,task2]);
+		expect(myCollection3.length()).to.equal(5);
+	});
+	it("Testing the new method", function() {
+		expect(myCollection2.length()).to.equal(2);
+		myCollection2.new();
+		expect(myCollection2.length()).to.equal(3);
+	});
+	it.skip("Testing the remove method", function() {
+		var myCollection4 = TaskCollection.new([task1, task2, task3]);
+		expect(myCollection4.length()).to.equal(3);
+		expect(myCollection4.remove(1).length()).to.equal(2);
+		expect(myCollection4.remove([2,3]).length()).to.equal(0);
+	});
+	it("Testing the filter method", function() {
+		//
+	});
+});
